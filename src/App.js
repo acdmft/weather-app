f58351bb2a7b42a0d57d8b1db8204f9ecbce0fe2
fusbudget <andrewsanser@gmail.com>
2022-02-23 15:02:14 +0100

    add WeatherContext in App.js

--- src/App.js
+++ src/App.js
@@ -1,18 +1,22 @@
 import { BrowserRouter, Switch, Route } from "react-router-dom";
 import Home from "./views/Home";
 import Favorites from "./views/Favorites";
+import { createContext } from "react";
 
+export const WeatherContext = createContext();
 
 function App() {
+  const value = {};
+
   return (
-    <>
-    <BrowserRouter>
-      <Switch>
-        <Route exact path="/" component={Home} />
-        <Route exact path="/favorites" component={Favorites} />
-      </Switch>
-    </BrowserRouter>
-    </>
+    <WeatherContext.Provider value={value}>
+      <BrowserRouter>
+        <Switch>
+          <Route exact path="/" component={Home} />
+          <Route exact path="/favorites" component={Favorites} />
+        </Switch>
+      </BrowserRouter>
+    </WeatherContext.Provider>
   );
 }
 
