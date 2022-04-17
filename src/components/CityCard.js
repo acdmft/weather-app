86c2609549d9b6b50204f3a1aa91c7cfd9341846
fusbudget <andrewsanser@gmail.com>
2022-04-17 16:04:56 +0200

    add WeatherAppContext and removeFromFavorites function

--- src/components/CityCard.js
+++ src/components/CityCard.js
@@ -1,8 +1,12 @@
 import { useEffect, useState } from "react";
+import { useContext } from "react";
+import { WeatherAppContext } from "../App";
 
 // fetch weather for the city
 export default function CityCard(props) {
   const [cityWeather, setWeather] = useState([]);
+  const appContext = useContext(WeatherAppContext);
+
   useEffect(() => {
     fetch(
       `https://api.openweathermap.org/data/2.5/forecast?q=${props.cityName},fr&appid=0385b2073dd106cd2ae5019621c6b1ba`
@@ -17,8 +21,20 @@ export default function CityCard(props) {
       );
   }, []);
 
-  return <>
-  <h3>{props.cityName}</h3>
-  <p>temperature: {cityWeather} </p>
-  </>;
+  const removeFromFavorites = (cityId) => {
+    appContext.favCities.splice(cityId, 1);
+    console.log(appContext.favCities);
+  };
+
+  return (
+    <>
+      <h3>{props.cityName}</h3>
+      <p>temperature: {cityWeather} </p>
+      <button
+        onClick={props.onClick}
+      >
+        -
+      </button>
+    </>
+  );
 }
--- src/views/Favorites.js
+++ src/views/Favorites.js
@@ -1,4 +1,4 @@
-import { useContext } from "react";
+import { useContext, useEffect } from "react";
 // components
 import Navbar from "../components/Navbar";
 import Footer from "../components/Footer";
@@ -8,14 +8,24 @@ import { WeatherAppContext } from "../App";
 
 export default function Favorites() {
   const appContext = useContext(WeatherAppContext);
+  
+  useEffect(() => {
+    console.log(appContext.favCities);
+  }, [appContext.length]);
+
+  const removeFavorite = (cityId) => {
+    appContext.favCities.splice(cityId, 1);
+    console.log(appContext.favCities);
+  };
+
   return (
     <>
       <Navbar />
       <h1>Favorites</h1>
       {appContext.favCities.length > 0 ? (
         <>
-          {appContext.favCities.map((city) => {
-            return ( <CityCard cityName={city} />)
+          {appContext.favCities.map((city, i) => {
+            return ( <CityCard cityName={city} cityId={i} key={i} onClick={() => {removeFavorite(i)}} /> ) 
           })}
         </>
       ) : (
