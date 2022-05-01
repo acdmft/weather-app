5de26782b10650fb56a28d0765f29dd8ecb9aea1
fusbudget <andrewsanser@gmail.com>
2022-05-01 13:43:25 +0200

    add styles to the CityCard component

--- src/components/CityCard.js
+++ src/components/CityCard.js
@@ -6,20 +6,25 @@ import { FavoriteCitiesContext } from "../App";
 
 export default function CityCard({weather, onClick, children}) {
   return (
-    <div className="flex flex-col w-96 cityCard">
-      <div className="z-10" >
+    <div className="flex flex-col w-96 cityCard border border-hidden rounded-md transition ease-in-out delay-200 hover:-translate-y-1 hover:scale-105 duration-300">
+      <div className="z-10 pt-8 text-gray-100 font-bold md:leading-10" >
       {/* city name */}
-      <h3>{weather.name}</h3>
+      <h2 className="text-center text-xl mb-4">{weather.name}</h2>
       {/* weather type */}
-      <p>description: {weather.weather[0].description}</p>
-      <p>temperature: {weather.main.temp} °C</p>
-      <p>humidity: {weather.main.humidity}</p>
-      <p>wind: {weather.wind.speed}</p>
+      <p className="text-center">{weather.weather[0].description}</p>
+      <p className="text-center">{weather.main.temp} °C</p>
+      <p className="text-center">humidity: {weather.main.humidity}</p>
+      <p className="text-center">wind: {weather.wind.speed}</p>
+       {/* ************ BUTTON ADD TO FAVORITES *****************/}
+      <div className="flex flex-row justify-around pt-5 mb-8">
       <button
         onClick={onClick}
+        className = "btn-primary"
       >
         {children}
       </button>
+
+      </div>
 
       </div>
     </div>
